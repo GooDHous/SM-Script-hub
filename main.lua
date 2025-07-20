a9b9e61e09b5f0357119b2cda412f3d415b829f6
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-07-20 23:30:30 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
---==[ Load SMUI UI ]==--
-local SMUI = loadstring(game:HttpGet('https://raw.githubusercontent.com/GooDHous/SM-Scripts/refs/heads/main/libui.lua'))()
+--==[ Load RayField UI ]==--
+local RayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/GooDHous/SM-Scripts/refs/heads/main/libui.lua'))()
 
 --==[ Create Window ]==--
-local Window = SMUI:CreateWindow({
+local Window = RayField:CreateWindow({
     Name = "SM-Script Hub",
     LoadingTitle = "Universal script for SM-Script Hub",
     LoadingSubtitle = "Made by SM-Team",
@@ -141,7 +141,7 @@ universal:CreateButton({
 settingsTab:CreateButton({
     Name = "Unload The GUI",
       Callback = function()
-        SMUI:Destroy()
+        RayField:Destroy()
         task.wait(1)
         loadstring(game:HttpGet('https://raw.githubusercontent.com/GooDHous/SM-Scripts/refs/heads/main/libui.lua'))()
     end,
@@ -149,7 +149,7 @@ settingsTab:CreateButton({
 
 --==[ Credits Tab ]==--
 creditsTab:CreateLabel("Made by SM-Team")
-creditsTab:CreateLabel("UI Framework: SMUI (RayField Fork)")
+creditsTab:CreateLabel("UI Framework: RayField (RayField Fork)")
 creditsTab:CreateLabel("Special thanks to you for using it!")
 
 succ_loaded = true
